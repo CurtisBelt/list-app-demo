251b76186ad28cc4f07d42c99d2e5c020f4fcaee
Curtis Belt <curtis@curtisbelt.dev>
2019-08-10 23:05:17 -0500

    feat: add netlify function items-create

---
+++ functions/items-create.js
@@ -0,0 +1,27 @@
+import faunadb from 'faunadb'
+
+const query = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_CLIENT_SECRET
+})
+
+exports.handler = (event) => {
+  const data = JSON.parse(event.body)
+  const payload = {
+    data
+  }
+  return client
+    .query(query.Create(query.Ref('classes/items'), payload))
+    .then((response) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      }
+    })
+    .catch((error) => {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
+}
